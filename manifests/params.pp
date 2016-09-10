c6eb17f1a96306edff85a9fe2d348279184f97f1
James Netherton <jamesnetherton@users.noreply.github.com>
2016-09-10 08:49:07 +0100

    Fixes #16. Ensure chrome yum repo file is identical to the one packaged in the RPM (#17)

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class google_chrome::params() {
   $ensure           = installed
   $version          = 'stable'
   $package_name     = 'google-chrome'
-  $repo_gpg_key     = 'http://dl-ssl.google.com/linux/linux_signing_key.pub'
+  $repo_gpg_key     = 'https://dl.google.com/linux/linux_signing_key.pub'
   $repo_gpg_key_id  = '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991'
   $repo_name        = 'google-chrome'
 
@@ -11,10 +11,10 @@ class google_chrome::params() {
       if $::operatingsystemmajrelease == 6 {
         fail('Operating system not supported by Google Chrome')
       }
-      $repo_base_url = 'http://dl.google.com/linux/chrome/rpm/stable/$basearch'
+      $repo_base_url = 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
     }
     'Suse' : {
-      $repo_base_url = 'http://dl.google.com/linux/chrome/rpm/stable/$basearch'
+      $repo_base_url = 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
     }
     'Debian': {
       $repo_base_url = '[arch=amd64] http://dl.google.com/linux/chrome/deb/'
