0b471889fa616570b893e58184c18ed309640af9
Steve ESSO <stivesso@gmail.com>
2017-12-07 09:51:59 +0100

    Update init.pp to add Data types
    Update init.pp to add Data types

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,11 +12,25 @@
 #  include 'google_chrome'
 #
 #  class { google_chrome:
-#    version => 'unstable',
+#    ensure           => 'installed',
+#    version          => 'unstable',
+#    package_name     => 'google-chrome',
+#    repo_gpg_key     => 'https://dl.google.com/linux/linux_signing_key.pub',
+#    repo_gpg_key_id  => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+#    repo_name        => 'google-chrome',
+#    defaults_file    => '/etc/default/google-chrome',
+#    repo_base_url    => 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
 #  }
 #
 #  class { google_chrome:
-#    version => 'beta',
+#    ensure           => 'installed',
+#    version          => 'beta',
+#    package_name     => 'google-chrome',
+#    repo_gpg_key     => 'https://dl.google.com/linux/linux_signing_key.pub',
+#    repo_gpg_key_id  => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+#    repo_name        => 'google-chrome',
+#    defaults_file    => '/etc/default/google-chrome',
+#    repo_base_url    => 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
 #  }
 #
 # === Copyright
@@ -24,17 +38,15 @@
 # Copyright 2014 James Netherton
 #
 class google_chrome(
-  $ensure           = $google_chrome::params::ensure,
-  $version          = $google_chrome::params::version,
-  $package_name     = $google_chrome::params::package_name,
-  $repo_gpg_key     = $google_chrome::params::repo_gpg_key,
-  $repo_gpg_key_id  = $google_chrome::params::repo_gpg_key_id,
-  $repo_name        = $google_chrome::params::repo_name,
-  $defaults_file    = $google_chrome::params::defaults_file,
-  $repo_base_url    = $google_chrome::params::repo_base_url
+  String $ensure                                    = $google_chrome::params::ensure,
+  Pattern[/^stable/, /^unstable/, /^beta/] $version = $google_chrome::params::version,
+  String $package_name                              = $google_chrome::params::package_name,
+  String $repo_gpg_key                              = $google_chrome::params::repo_gpg_key,
+  String $repo_gpg_key_id                           = $google_chrome::params::repo_gpg_key_id,
+  String $repo_name                                 = $google_chrome::params::repo_name,
+  String $defaults_file                             = $google_chrome::params::defaults_file,
+  String $repo_base_url                             = $google_chrome::params::repo_base_url
 ) inherits google_chrome::params {
-
-  validate_re($version, ['^stable','^unstable','^beta'])
 
   class { 'google_chrome::config': }
   -> class { 'google_chrome::install': }
