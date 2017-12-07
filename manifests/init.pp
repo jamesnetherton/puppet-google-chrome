db960e310c99c4beb0824b914c6a84a29d1c22eb
Steve ESSO <stivesso@gmail.com>
2017-12-07 11:07:28 +0100

    Update init.pp to remove Data Types
    Cause incompatibility Issues when running Unit Test (probably need to update the Unit Test).

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,15 +38,17 @@
 # Copyright 2014 James Netherton
 #
 class google_chrome(
-  String $ensure                                    = $google_chrome::params::ensure,
-  Pattern[/^stable/, /^unstable/, /^beta/] $version = $google_chrome::params::version,
-  String $package_name                              = $google_chrome::params::package_name,
-  String $repo_gpg_key                              = $google_chrome::params::repo_gpg_key,
-  String $repo_gpg_key_id                           = $google_chrome::params::repo_gpg_key_id,
-  String $repo_name                                 = $google_chrome::params::repo_name,
-  String $defaults_file                             = $google_chrome::params::defaults_file,
-  String $repo_base_url                             = $google_chrome::params::repo_base_url
+  $ensure           = $google_chrome::params::ensure,
+  $version          = $google_chrome::params::version,
+  $package_name     = $google_chrome::params::package_name,
+  $repo_gpg_key     = $google_chrome::params::repo_gpg_key,
+  $repo_gpg_key_id  = $google_chrome::params::repo_gpg_key_id,
+  $repo_name        = $google_chrome::params::repo_name,
+  $defaults_file    = $google_chrome::params::defaults_file,
+  $repo_base_url    = $google_chrome::params::repo_base_url
 ) inherits google_chrome::params {
+
+  validate_re($version, ['^stable','^unstable','^beta'])
 
   class { 'google_chrome::config': }
   -> class { 'google_chrome::install': }
